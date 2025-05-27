473129994b9f5d5210c689cc9102b7509e6d2c98
Luthando Gubevu <lgubevu@gmail.com>
2025-05-27 17:19:06 +0000

    Make the footer full length in width

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,7 +1,7 @@
 
 export function Footer() {
   return (
-    <footer className="py-6 md:px-8 md:py-0 bg-background/30 border-t">
+    <footer className="py-6 md:py-0 bg-background/30 border-t">
       <div className="container flex flex-col items-center justify-between gap-4 md:h-20 md:flex-row">
         <p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
           © {new Date().getFullYear()} Fundanii Ai. All rights reserved.
